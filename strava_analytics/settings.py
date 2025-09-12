ba6c0947ab338c633a7da5e30e8d891c27401892
Phil <philjim@outlook.com>
2025-09-12 16:31:41 +0100

    Fix collectstatic issue - Set STATIC_ROOT permanently, enable collectstatic in run command
    - STATIC_ROOT was missing, causing Django admin static files to fail
    - collectstatic now works properly in production
    - Tested locally with successful static file collection (130 files)

--- strava_analytics/settings.py
+++ strava_analytics/settings.py
@@ -150,10 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# For production without collectstatic
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
